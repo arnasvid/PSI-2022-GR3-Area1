d2369019d73f48cc8d8e271cce09b2f17361ea83
vstraigis <val.straigis@gmail.com>
2022-11-03 16:27:52 +0200

    Update format check

--- Area1/src/roofdataextractor.py
+++ Area1/src/roofdataextractor.py
@@ -1,18 +1,18 @@
-### Version 1.0
-
-# Python 3 only
-
-# Defused xml prachekina daug attacku, kurios gali but xml'e, tai pasiskaitysim ka tiksliai kai baigsim
-
 from xmljson import badgerfish as bf
 from defusedxml.ElementTree import fromstring
+from collections import OrderedDict
 import defusedxml.ElementTree as ET
 import json
 
+bf = bf(dict_type = OrderedDict)
 
 def check_format(file):
+    #check if the file is correctly formatted
     try:
-        ET.parse(file)
+        tree = ET.parse(file)
+        tree.getroot() == "DATA_EXPORT"
+        tree.getroot()[2].tag == "ADDRESS"
+        #check if third child's child is named "ROOF"
         return True
     except ET.ParseError:
         # if the file is not well-formed, print that is not well-formed
@@ -21,19 +21,19 @@ def check_format(file):
 
 
 
-# define a function to convert xml to json
+#define a function to convert xml to json
 def xml_to_json(file):
-
- if file.endswith(".xml"):
-    # if the file is well-formed...
+# if the file is well-formed...
     if check_format(file):
-        # get file name   
+    # get file name   
         name = str.split(file, ".")[-2]
         # open the XML file, convert to json and dump into a new file
         with open(file, "r") as input:
             jsonOut = bf.data(fromstring(input.read()))
             with open(name + ".json","w+") as newFile:
                 json.dump(jsonOut, newFile, ensure_ascii=False)
+                
+
 
 
 def main():
