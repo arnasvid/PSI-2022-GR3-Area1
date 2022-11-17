637639d004bff6be81be86ab370c1b19487cc720
arnasvid <arnas.vidziunas@gmail.com>
2022-11-17 14:31:56 +0200

    Add testing directory

---
+++ Area1/src/tests/test_roofdataextractor.py
@@ -0,0 +1,7 @@
+# write test if the stogas.xml file is well-formated
+
+# from roofdata.roofdataextractor import check_format
+
+
+# def test_check_format():
+#     assert check_format("./XMLFIles/stogas.xml") == True
