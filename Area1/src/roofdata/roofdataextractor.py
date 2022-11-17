23777829be900914b7deae3bca63f1b7ef171d24
arnasvid <arnas.vidziunas@gmail.com>
2022-11-17 18:03:55 +0200

    Add 5 working unit tests, in roofdataextractor.py changed path to the first file so now it runs without any problem (I wish)

--- Area1/src/roofdata/roofdataextractor.py
+++ Area1/src/roofdata/roofdataextractor.py
@@ -4,7 +4,7 @@ import xml_to_json
 
 def main():
     #file = input("Iveskite failo pavadinima arba kelia iki jo: ")
-    file="src\XMLFIles\stogas.xml"
+    file="src/XMLFiles/stogas.xml"
     print(extract_roof_data.erd(xml_to_json.xtj(file)))
     #extract_roof_data.erd(xml_to_json.xtj(file))
     
--- Area1/src/tests/test_roofdataextractor.py
+++ Area1/src/tests/test_roofdataextractor.py
@@ -1,7 +1,38 @@
-# write test if the stogas.xml file is well-formated
+import unittest
+from unittest import TestCase
+from roofdata.check_format import cf
+from roofdata.xml_to_json import xtj
+from roofdata.extract_roof_data import erd
 
-# from roofdata.roofdataextractor import check_format
+
+class MyTest(TestCase):
+    def test_successful_check_format(self):
+        self.assertTrue(cf("src/XMLFiles/stogas.xml"))
+
+    def test_failed_check_format(self):
+        self.assertFalse(cf("src/XMLFiles/bad_stogas.xml"))
+
+    def test_check_format_raises_file_not_found_error(self):
+        with self.assertRaises(FileNotFoundError):
+            cf("src/XMLFiles/doesnt_exist.xml")
+
+    def test_successful_xml_to_json(self):
+        self.assertTrue(xtj("src/XMLFiles/stogas.xml"))
+
+    def test_failed_xml_to_json(self):
+        self.assertFalse(xtj("src/XMLFiles/bad_stogas.xml"))
+
+    # def test_successful_extract_roof_data(self):
+    #     try:
+    #         self.assertTrue(erd(xtj("src/XMLFiles/stogas.xml")))
+    #     except:
+    #         print("Failed to extract roof data")
+    #     raise Exception("Failed to extract roof data")
+
+    # def test_failed_extract_roof_data(self):
+    #     self.assertRaises(Exception)
+    #     erd(xtj("src/XMLFiles/bad_stogas.xml"))
 
 
-# def test_check_format():
-#     assert check_format("./XMLFIles/stogas.xml") == True
+if __name__ == '__main__':
+    unittest.main()
